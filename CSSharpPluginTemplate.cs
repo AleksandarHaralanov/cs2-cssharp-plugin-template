94b6fb51b61cd29ecac1c25fe2a5efc0d2da6725
AleksandarHaralanov <aleksandar.haralanov01@gmail.com>
2025-03-16 20:24:47 +0200

    Add project files.

---
+++ CSSharpPluginTemplate.cs
@@ -0,0 +1,24 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+
+namespace CSSharpPluginTemplate
+{
+    [MinimumApiVersion(305)] // As of 3/16/2025
+    public class CSSharpPluginTemplate : BasePlugin
+    {
+        public override string ModuleName => "PLUGIN_NAME";
+        public override string ModuleVersion => "CHANGE_PLUGIN_VERSION";
+        public override string ModuleAuthor => "CHANGE_PLUGIN_AUTHOR";
+        public override string ModuleDescription => "CHANGE_PLUGIN_DESCRIPTION";
+
+        public override void Load(bool hotReload)
+        {
+            // Load logic
+        }
+
+        public override void Unload(bool hotReload)
+        {
+            // Unload logic
+        }
+    }
+}
